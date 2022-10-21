b3d0175d79f63dc41378b4294b83b112e9c21119
Григоренко Александр Игоревич <aigrigorenko@mtsbank.ru>
2022-10-21 11:43:43 +0500

    Lesson-26

---
+++ instagramProject/instagramProject/ActionSceen/Cells/CommentTableViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  CommentTableViewCell.swift
+//  instagramProject
+//
+//  Created by Григоренко Александр Игоревич on 21.10.2022.
+//
+
+import UIKit
+
+/// Ячейка с комментарием.
+final class CommentTableViewCell: UITableViewCell {}
